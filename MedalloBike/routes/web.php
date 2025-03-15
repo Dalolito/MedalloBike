facfce340f4f158c43d54e7c0be09a9be1b68678
David Lopera <dein4267@gmail.com>
2025-03-15 11:13:57 -0500

    correction in routes

--- MedalloBike/routes/web.php
+++ MedalloBike/routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 $AdminControllerRoute = 'App\Http\Controllers\Admin\AdminProductController';
 
-Route::get('/admin/product/create', $controllerRoute.'@create')->name('admin.product.create');
-Route::post('/admin/product/save', $controllerRoute.'@save')->name('admin.product.save');
+Route::get('/admin/product/create', $AdminControllerRoute.'@create')->name('admin.product.create');
+Route::post('/admin/product/save', $AdminControllerRoute.'@save')->name('admin.product.save');
