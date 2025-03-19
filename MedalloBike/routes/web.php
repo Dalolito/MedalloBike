06b03c61df941c63942cd77d89dda5fa35782a69
David Lopera <dein4267@gmail.com>
2025-03-18 19:40:17 -0500

    merge conflicts corrections

--- MedalloBike/routes/web.php
+++ MedalloBike/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 $AdminProductControllerRoute = 'App\Http\Controllers\Admin\AdminProductController';
+$HomeControllerRoute = 'App\Http\Controllers\HomeController';
 
+Route::get('/', $HomeControllerRoute.'@index')->name('home.index');
 Route::get('/admin/product/create', $AdminProductControllerRoute.'@create')->name('admin.product.create');
 Route::post('/admin/product/save', $AdminProductControllerRoute.'@save')->name('admin.product.save');
 Route::get('/admin/product/list', $AdminProductControllerRoute.'@list')->name('admin.product.list');
 Route::get('/admin/product/show/{id}', $AdminProductControllerRoute.'@show')->name('admin.product.show');
+
+Auth::routes();
