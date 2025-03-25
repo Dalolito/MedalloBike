28d4dd535f1f6be1f876bc99d6681cb144460a91
Juan Camilo <jcvillac@eafit.edu.co>
2025-03-24 19:43:01 -0500

    migración agregada

---
+++ MedalloBike/database/migrations/2025_03_25_002804_create_reviews_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('qualification');
+            $table->string('review', 255);
+            $table->boolean('state')->default(false);
+            $table->timestamps(); 
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
